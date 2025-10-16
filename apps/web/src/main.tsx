1353ae05420171a082fdfa2a4af8fe65cd3f4729
damienPilat <dspilat@hotmail.com>
2025-10-16 19:16:53 +0100

    created stationCard with fetch and styling

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,27 +1,16 @@
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Header, Counter } from "@repo/ui";
+import {Header} from "@repo/ui";
+import StationContainer from "./components/Station/StationContainer";
 
 const App = () => (
-  <div>
-    <Header title="Playground" />
-    <p>Exploring how to work on monorepos using turborepo for the setup. Creating webapps in React/TS, built with Vite, and deployed to Vercel</p>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-    </a>
+    <div>
+        <Header title="Playground"/>
+        <p>Exploring how to work on monorepos using turborepo for the setup. Creating webapps in React/TS, built with
+            Vite, and deployed to Vercel</p>
 
-    <div className="card">
-      <Counter />
+        <StationContainer />
     </div>
-  </div>
 );
 
-createRoot(document.getElementById("app")!).render(<App />);
+createRoot(document.getElementById("app")!).render(<App/>);
